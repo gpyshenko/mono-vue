ad0f60c6fbed500c3ad8698da31f211aa6b24026
gpyshenko <gpyshenko@gmail.com>
2018-12-25 00:07:11 +0500

    Added Header and Navigation

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header class="header">
+        <div class="header-nav">
+            <app-nav></app-nav>
+        </div>
+        <div class="header-burger"></div>
+    </header>
+</template>
+
+<script>
+import Nav from './Nav.vue'
+export default {
+    components: {
+        appNav: Nav
+    }
+}
+</script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="nav">
+    <ul class="nav-list">
+      <li class="nav-item" v-for="(link, index) in links" :key="index">
+        <a :href="link.url" class="nav-link">{{link.title}}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    "title": "Главная",
+                    "url": "index"
+                },
+                {
+                    "title": "О нас",
+                    "url": "about"
+                },
+                {
+                    "title": "Разработки",
+                    "url": "portfolio"
+                },
+                {
+                    "title": "Локация",
+                    "url": "maps"
+                },
+                {
+                    "title": "Контактики",
+                    "url": "contacts"
+                }
+            ]
+        }
+    }
+}
+</script>
